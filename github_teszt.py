8543c5e6f0778c648738e3ce9ead47c4f611b00a
Ferenc Piller <fpiller@gmail.com>
2026-09-17 08:42:21 +0200

    Implement even number check in github_teszt.py
    Added a check to determine if there are even numbers in the list.

--- github_teszt.py
+++ github_teszt.py
@@ -12,8 +12,14 @@ atlag = atlag / len(szamok)
 
 print(f"Átlag: {atlag}")
 
+# Van-e páros szám?
+i = 0
+while i < len(szamok) and szamok[i] % 2 != 0:
+    i += 1
 
-
-
+if i < len(szamok):
+    print("Van páros szám!")
+else:
+    print("Nincs páros szám!")
 
 
